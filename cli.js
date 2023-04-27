5e9cf80f2e14c212b10000c73d19df7858a92ae0
Madeleine Genova <madiane@unc.edu>
2023-04-27 16:17:45 -0400

    Fixed -j without additional values

--- cli.js
+++ cli.js
@@ -77,8 +77,14 @@ async function main() {
   let data = await response.json()
   
   if (JSON_ONLY) {
-    console.log(data)
-    process.exit(0)
+    if (!options.latitude || !options.longitude) {
+      console.log("Latitude must be in range.\nLongitude must be in range.")
+      process.exit(1)
+    }
+    else {
+      console.log(data)
+      process.exit(0)
+    }
   }
   
   let precip = data.daily.precipitation_hours[options.day]
